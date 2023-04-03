3076c55f3ffe78cff84bf8585a89a7a2a7501ff8
Don Heshanthaka <heshanthaka007@gmail.com>
2023-04-04 00:51:06 +0530

    Implement Docker configuration

--- src/api/weather_api.js
+++ src/api/weather_api.js
@@ -6,7 +6,7 @@
 const getWeatherDataAPI = async (cityCode) => {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?id=${cityCode}&units=metric&appid=${
-      import.meta.env.VITE_APP_OPENWEATHER_API_KEY
+      import.meta.env.VITE_APP_OPENWEATHER_API_KEY ? import.meta.env.VITE_APP_OPENWEATHER_API_KEY : process.env.VITE_APP_OPENWEATHER_API_KEY
     }`
   )
   const data = await response.json()
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8000,
+  },
+  define: {
+    'process.env': {
+      VITE_APP_OPENWEATHER_API_KEY: JSON.stringify(process.env.VITE_APP_OPENWEATHER_API_KEY),
+    },
+  },
 })
