df3e10fe7ed4ea2ed6eac6736a23edc802c01ff0
Don Heshanthaka <heshanthaka007@gmail.com>
2023-04-16 03:52:54 +0530

    Code Refactor: random_hue.js

--- src/utils/random_hue.js
+++ src/utils/random_hue.js
@@ -3,11 +3,11 @@
  * @returns {number} A random hue value between 0 and 360 (inclusive) that is a multiple of 10.
  */
 const randomHueValue = () => {
-  const min = 0;
-  const max = 36;
-  const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-  const hueValue = randomNum * 10;
-  return hueValue;
+  const min = 0
+  const max = 36
+  const randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+  const hueValue = randomNum * 10
+  return hueValue
 }
 
 export default randomHueValue
